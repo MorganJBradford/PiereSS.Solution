c26595571d24d969c9182a1248400b92d3d37378
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-26 10:59:55 -0500

    rename controllers. Connect AccountController to db/identity user and sign in managers

--- PierreSS.cs/Controllers/AccountController.cs
+++ PierreSS.cs/Controllers/AccountController.cs
@@ -2,6 +2,9 @@ namespace PierreSS.Controllers
 {
   public class AccountController : Controller
   {
-
+    private readonly PierreSSContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    
   }
 }
