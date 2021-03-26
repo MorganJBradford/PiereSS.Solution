bf4a7b2604adb37b2f72ddc264b2434b6b855f06
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-26 13:29:49 -0500

    connect database and pass into constructor

--- Pierre/Controllers/TreatsController.cs
+++ Pierre/Controllers/TreatsController.cs
@@ -5,5 +5,11 @@ namespace Pierre.Controllers
   public class TreatsController : Controller
   {
 
+    private readonly PierreContext _db;
+
+    public TreatsController(PierreContext db)
+    {
+      _db = db;
+    }
   }
 }
