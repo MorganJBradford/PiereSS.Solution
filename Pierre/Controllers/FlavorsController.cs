097dea489bff1881a00270bacf6b4dab3dfbfd8b
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-26 13:44:55 -0500

    connect database; pass into constructor, add [Get]Index route

--- Pierre/Controllers/FlavorsController.cs
+++ Pierre/Controllers/FlavorsController.cs
@@ -4,6 +4,16 @@ namespace Pierre.Controllers
 {
   public class FlavorsController : Controller
   {
+    private readonly PierreContext _db;
 
+    public FlavorsController(PierreContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Flavors.ToList());
+    }
   }
 }
