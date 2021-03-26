4acf9115aba8663c9ebe02a1b1e26a52119399ed
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-26 16:47:28 -0500

    pass list of all flavors and all treats to ViewBag

--- Pierre/Controllers/HomeController.cs
+++ Pierre/Controllers/HomeController.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Pierre.Models;
+using System.Linq;
 
 namespace Pierre.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierreContext _db;
+
+    public HomeController(PierreContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.allTreats = _db.Treats.ToList();
+      ViewBag.allFlavors = _db.Flavors.ToList();
       return View();
     }
   }
