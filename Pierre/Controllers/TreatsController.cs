a74ba4bf8fe79e6a349589b7ca5d2cf265f81c8c
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-26 16:25:03 -0500

    return to previous layout

--- Pierre/Controllers/TreatsController.cs
+++ Pierre/Controllers/TreatsController.cs
@@ -42,8 +42,11 @@ namespace Pierre.Controllers
     }
 
     [HttpPost]
-    public ActionResult Create(Treat treat, int FlavorId)
+    public async Task<ActionResult> Create(Treat treat, int FlavorId)
     {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      treat.User = currentUser;
       _db.Treats.Add(treat);
       _db.SaveChanges();
       if (FlavorId != 0)
