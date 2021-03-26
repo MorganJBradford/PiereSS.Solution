b6aefd8d1beddc09474458a128b822b57d9e9220
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-26 10:54:04 -0500

    connect Flavor, Treat, FlavorTreat to db

--- PierreSS.cs/Models/PierreSSContext.cs
+++ PierreSS.cs/Models/PierreSSContext.cs
@@ -5,8 +5,9 @@ namespace PierreSS.Models
 {
   public class PierreSSContext : IdentityDbContext<ApplicationUser>
   {
-
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
     public PierreSSContext(DbContextOptions options) : base(options) { }
-
   }
 }
