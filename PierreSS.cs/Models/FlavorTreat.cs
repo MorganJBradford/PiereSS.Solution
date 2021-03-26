1261138cdae3edc7a54c8b4c9513ef28213271fb
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-26 10:50:56 -0500

    add FlavorTreatId. Add id and objects for joined classes

--- PierreSS.cs/Models/FlavorTreat.cs
+++ PierreSS.cs/Models/FlavorTreat.cs
@@ -2,6 +2,10 @@ namespace PierreSS.Models
 {
   public class FlavorTreat
   {
-    // properties, constructors, methods, etc. go here
+    public int FlavorTreatId { get; set; }
+    public int FlavorId { get; set; }
+    public int TreatId { get; set; }
+    public virtual Flavor Flavor { get; set; }
+    public virtual Treat Treat { get; set; }
   }
 }
