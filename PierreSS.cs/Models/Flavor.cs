43719f788f0145075494f90ffdb2eaad403659a7
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-26 10:47:50 -0500

    remove set from join entities

--- PierreSS.cs/Models/Flavor.cs
+++ PierreSS.cs/Models/Flavor.cs
@@ -11,6 +11,6 @@ namespace PierreSS.Models
 
     public int FlavorId { get; set; }
     public string Name { get; set; }
-    public virtual ICollection<FlavorTreat> JoinEntities { get; set; }
+    public virtual ICollection<FlavorTreat> JoinEntities { get; }
   }
 }
